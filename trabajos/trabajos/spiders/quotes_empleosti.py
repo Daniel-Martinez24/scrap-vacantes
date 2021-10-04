22f44a2ef8d58a0b51e67de4e0cec4368c369b24
Daniel Martinez <dan24diaz@gmail.com>
2021-10-04 15:15:55 -0500

    añadido salario, lugar, tipo y trabajo, bug en tipo

--- trabajos/trabajos/spiders/quotes_empleosti.py
+++ trabajos/trabajos/spiders/quotes_empleosti.py
@@ -21,18 +21,23 @@ class QuotesSpider(scrapy.Spider):
             yield response.follow(link, callback=self.parse_pages)
       
     def parse_pages(self, response):
-        # scores = response.xpath('//span[@title="Score"]/text()').getall()
-        # dates = response.xpath('//span[@class="remain-time"]/text()').getall()
         titles = response.xpath('//h1[@class="section-title"]/text()').getall()
+        salary = response.xpath('//*[@id="vacancy-content"]/ul/li[1]/p[@class="gray-subarea-title"]/text()').getall()
+        place = response.xpath('//*[@id="vacancy-content"]/ul/li[2]/p[@class="gray-subarea-title"]/text()').getall()
+        type = response.xpath('//*[@id="vacancy-content"]/ul/li[3]/p[@class="gray-subarea-title"]/text()').getall()
         english = response.xpath('//*[@id="vacancy-content"]/ul/li[4]/p[@class="gray-subarea-title"]/text()').getall()
         
+
         # scores = [score.strip() for score in scores ]
         # dates = [date.strip() for date in dates]
         
         for posicion in range(len(english)):
             yield {
-                'titulo ': titles[posicion],
-                'english ': english[posicion]
+                'titulo': titles[posicion],
+                'salario': salary[posicion],
+                'lugar': place[posicion],
+                'tipo_trabajo': type[posicion],
+                'nivel_ingles': english[posicion]
                 # 'score': scores[posicion],
                 # 'date': dates[posicion]
             }
