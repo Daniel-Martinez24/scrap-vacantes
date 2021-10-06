133ae8fc583f4f6f0734cdde04315b40da3c7597
Daniel Martinez <dan24diaz@gmail.com>
2021-10-05 19:43:38 -0500

    añadida empresa, simplificado código

--- trabajos/trabajos/spiders/quotes_empleosti.py
+++ trabajos/trabajos/spiders/quotes_empleosti.py
@@ -21,23 +21,25 @@ class QuotesSpider(scrapy.Spider):
             yield response.follow(link, callback=self.parse_pages)
       
     def parse_pages(self, response):
-        titles = response.xpath('//h1[@class="section-title"]/text()').getall()
-        salary = response.xpath('//*[@id="vacancy-content"]/ul/li[1]/p[@class="gray-subarea-title"]/text()').getall()
-        place = response.xpath('//*[@id="vacancy-content"]/ul/li[2]/p[@class="gray-subarea-title"]/a/text()').getall()
-        type = response.xpath('//*[@id="vacancy-content"]/ul/li[3]/p[@class="gray-subarea-title"]/text()').getall()
-        english = response.xpath('//*[@id="vacancy-content"]/ul/li[4]/p[@class="gray-subarea-title"]/text()').getall()
+        titles = response.xpath('//h1[@class="section-title"]/text()').get()
+        salary = response.xpath('//*[@id="vacancy-content"]/ul/li[1]/p[@class="gray-subarea-title"]/text()').get()
+        place = response.xpath('//*[@id="vacancy-content"]/ul/li[2]/p[@class="gray-subarea-title"]/a/text()').get()
+        type = response.xpath('//*[@id="vacancy-content"]/ul/li[3]/p[@class="gray-subarea-title"]/text()').get()
+        english = response.xpath('//*[@id="vacancy-content"]/ul/li[4]/p[@class="gray-subarea-title"]/text()').get()
+        company = response.xpath('//*[@id="searchable-enterprise"]/text()').get()
         
 
         # scores = [score.strip() for score in scores ]
         # dates = [date.strip() for date in dates]
         
-        for posicion in range(len(english)):
-            yield {
-                'titulo': titles[posicion],
-                'salario': salary[posicion],
-                'lugar': place[posicion],
-                'tipo_trabajo': type[posicion],
-                'nivel_ingles': english[posicion]
-                # 'score': scores[posicion],
-                # 'date': dates[posicion]
-            }
+        # for posicion in range(len(english)):
+        yield {
+            'titulo': titles,
+            'salario': salary,
+            'lugar': place,
+            'tipo_trabajo': type,
+            'nivel_ingles': english,
+            'empresa' : company,
+            # 'score': scores[posicion],
+            # 'date': dates[posicion]
+        }
